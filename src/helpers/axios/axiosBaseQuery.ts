3f7b76f9bb64ad0d048980b21c5847e1b8962b53
ayansheikh6600 <ayansheikh6600@gmail.com>
2024-05-08 22:51:36 +0500

    api timeout increase

--- src/helpers/axios/axiosBaseQuery.ts
+++ src/helpers/axios/axiosBaseQuery.ts
@@ -33,6 +33,7 @@ export const axiosBaseQuery =
         method,
         data,
         params,
+        timeout : 240000,
         headers: {
           "Content-Type": contentType || "application/json",
         },
